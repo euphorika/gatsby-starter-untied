1c20e5f595e3d33b1ca633a3b20695706334b976
Jan Winterfeld <jan@markenbegeisterung.de>
2018-09-13 11:15:32 +0200

    Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer style={{
+    textAlign: 'right',
+    background: 'rebeccapurple',
+    padding: '1.45rem 1.0875rem',
+    color: '#fff'
+  }}>
+    <span style={{fontSize: '12px'}}>&copy; 2018</span>
+  </footer>
+)
+
+export default Footer
