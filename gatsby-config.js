338aa6ac53146adc58df37d9aeb1f4def3c37e84
Jan Winterfeld <jan@markenbegeisterung.de>
2018-11-01 21:04:48 +0100

    adjust title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Starter United',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -8,7 +8,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
+        name: 'gatsby-starter-untied',
         short_name: 'starter',
         start_url: '/',
         background_color: '#663399',
