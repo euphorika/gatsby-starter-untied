02a3b4fec2c9b71623186bb1e83d7ecc00363407
Jan Winterfeld <jan@markenbegeisterung.de>
2018-11-03 22:23:26 +0100

    init cypress tests, test blog template

---
+++ cypress/integration/templates/blog-template-spec.js
@@ -0,0 +1,8 @@
+describe('My First Test', () => {
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('/very-first-blog-post/')
+    cy.get('h1').should('be.visible')
+    cy.get('h2').should('be.visible')
+    cy.get('div').should('be.visible')
+  })
+})
