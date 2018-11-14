2dd7185ac2bc155ea1d02332fdd086b14c8d083d
Jan Winterfeld <jan@markenbegeisterung.de>
2018-11-14 09:47:57 +0100

    Init Gatsby Navigation Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,7 @@ module.exports = {
     },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-stylus',
+    'gatsby-plugin-navigation',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
