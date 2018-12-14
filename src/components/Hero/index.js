adb236aae0b33f77bca5fcc49d16373088e2e15e
Jan Winterfeld <jan@markenbegeisterung.de>
2018-12-14 21:21:05 +0100

    Boilerplate Hero Section

---
+++ src/components/Hero/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import styles from './styles.module.styl'
+
+const Hero = () => (
+  <section className={styles.heroSection}>Hero Section</section>
+)
+
+export default Hero
--- src/pages/components.js
+++ src/pages/components.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import Layout from '../components/layout'
 
+import Hero from '../components/Hero/'
+
 const ComponentsPage = () => (
-  <Layout>Components</Layout>
+  <Layout>
+    <Hero />
+  </Layout>
 )
 
 export default ComponentsPage
