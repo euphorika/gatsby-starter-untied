e18010b419c606fdac196cf53823e218db07c6b9
Jan Winterfeld <jan@markenbegeisterung.de>
2019-01-15 17:31:04 +0100

    allow fluid image parameter for hero section, styling

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 
 import styles from './styles.module.styl'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
 
-const Hero = ({ children, headline, button }) => (
+const Hero = ({ children, headline, button, imgFluid }) => (
   <section className={styles.heroSection}>
-    <h2>{headline}</h2>
-    <p>{children}</p>
-    <div>
-      <button className={styles.button}>
-        <Link to={button.link}>
-          <span className={styles.text}>{button.text}</span>
-        </Link>
-      </button>
+    <div className={styles.background}>
+      <Img fluid={imgFluid} style={{height: '100%'}} imgStyle={{objectFit: 'cover'}} />
+    </div>
+    <div className={styles.info}>
+      <h2>{headline}</h2>
+      <p>{children}</p>
+      <div>
+        <button className={styles.button}>
+          <Link to={button.link}>
+            <span className={styles.text}>{button.text}</span>
+          </Link>
+        </button>
+      </div>
     </div>
   </section>
 )
