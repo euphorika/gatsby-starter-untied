fc7feb848d345468553cab54110a6c1851dd7185
Jan Winterfeld <jan@markenbegeisterung.de>
2019-01-16 15:53:59 +0100

    input type text, email, number

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -1,7 +1,35 @@
 import React from 'react'
 
+import Input from './input'
+
 import styles from './styles.module.styl'
 
-const Forms = () => <section className={styles.formsSection}>Forms</section>
+const Forms = () => {
+  const inputTypes = [
+    'text',
+    'email',
+    'number',
+  ]
+
+  return (
+    <section className={styles.formsSection}>
+      <div className={styles.inputContainer}>
+        {inputTypes.map((value, key) => (
+          <div key={key}>
+            <Input
+              type={value}
+              name={value}
+              label={value.toUpperCase()}
+              options={{
+                placeholder: value.toUpperCase(),
+                required: 'required',
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
 
 export default Forms
---
+++ src/components/Forms/input.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import styles from './input.module.styl'
+
+const Input = ({ value, name, type = 'text', label, options }) => {
+  const input = (
+    <>
+      <input
+        className={styles.input}
+        {...options}
+        value={value}
+        name={name}
+        type={type}
+      />
+      <span className={styles.afterInput} />
+    </>
+  )
+
+  if (label) {
+    return (
+      <label>
+        {label}
+        {input}
+      </label>
+    )
+  }
+
+  return input
+}
+
+export default Input
