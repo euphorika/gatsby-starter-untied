f049be2e91e3951bdc7ff6e39208e860133f200a
Jan Winterfeld <jan@markenbegeisterung.de>
2019-03-08 13:12:15 +0100

    pricingTable example

--- src/pages/components.js
+++ src/pages/components.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 
@@ -7,39 +6,20 @@ import HeroExample from '../components/examples/hero'
 import TeamMembersExample from '../components/examples/teamMembers'
 import CallToActionExample from '../components/examples/callToAction'
 import Slider from '../components/Slider/'
-import PricingTable from '../components/PricingTable/'
+import PricingTableExample from '../components/examples/pricingTable'
 import Forms from '../components/Forms/'
 import Video from '../components/Video/'
 import TestimonialsExample from '../components/examples/testimonial'
 import CalloutExample from '../components/examples/callout'
 
-const ComponentsPage = ({ data }) => {
-  const { locales, components } = data.site.siteMetadata
-  const { pricingTable } = components
-
+const ComponentsPage = () => {
   return (
     <Layout>
       <HeroExample />
       <TeamMembersExample />
       <CallToActionExample />
       <Slider />
-      {pricingTable.map((value, key) => {
-        const formattedPrice = new Intl.NumberFormat(locales, {
-          style: 'currency',
-          currency: value.currency,
-        }).format(value.price)
-
-        return (
-          <PricingTable
-            key={key}
-            headline={value.headline}
-            price={formattedPrice}
-            callToAction={value.callToAction}
-          >
-            {value.body}
-          </PricingTable>
-        )
-      })}
+      <PricingTableExample />
       <Forms />
       <Video />
       <TestimonialsExample />
@@ -49,25 +29,3 @@ const ComponentsPage = ({ data }) => {
 }
 
 export default ComponentsPage
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        locales
-        components {
-          pricingTable {
-            headline
-            price
-            currency
-            callToAction {
-              text
-              link
-            }
-            body
-          }
-        }
-      }
-    }
-  }
-`
