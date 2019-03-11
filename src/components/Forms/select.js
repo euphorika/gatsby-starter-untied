2c57368e637568d93fc6d7f12ca02e1b194fbe8f
Jan Winterfeld <jan@markenbegeisterung.de>
2019-03-11 15:40:44 +0100

    fix arrow position

--- src/components/Forms/select.js
+++ src/components/Forms/select.js
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types'
 import styles from './select.module.styl'
 
 const Select = ({ children, name, label, options }) => {
+  const requiredClass = options && options.required ? ` ${styles.required}` : ''
   const select = (
-    <div className={styles.selectBox}>
+    <div className={`${styles.selectBox}${requiredClass}`}>
       <select className={styles.element} name={name} {...options}>
         {children}
       </select>
