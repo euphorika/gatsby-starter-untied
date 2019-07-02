7f556fcf19c775492e1b40dad67a3c212d681c5c
Hussein Bawab <hussein@markenbegeisterung.de>
2019-07-02 18:25:54 +0200

    create different browser compatible video component

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styles from './styles.module.styl';
+import video_ele_webm from './elephants_dream.webm';
+import video_ele_mp4 from './elephants_dream.mp4';
 
-import styles from './styles.module.styl'
+function Video() {
+    const videos = [
+        {
+            src: video_ele_webm,
+            type: "video/webm"
+        },
+        {
+            src: video_ele_mp4,
+            type: "video/mp4"
+        }
+    ]
 
-const Video = () => <section className={styles.videoSection}>Video</section>
 
+
+
+    const displayVideos = videos.map((video, index) =>
+              <source src={video.src} type={video.type} />
+          )
+
+
+    return (
+      <video className={styles.videoSection} playsInline autoPlay controls>
+        {displayVideos}
+      </video>
+    )
+}
 export default Video
