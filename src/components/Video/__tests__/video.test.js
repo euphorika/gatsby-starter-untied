0055b888dde308346ce98c4786fb8a182d8035bc
Hussein Bawab <hussein@markenbegeisterung.de>
2019-07-09 12:07:35 +0200

    create Video Component add styles add webm-video and mp4-video modify tests

--- src/components/Video/__tests__/video.test.js
+++ src/components/Video/__tests__/video.test.js
@@ -2,9 +2,32 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import Video from '../'
 
-describe('Video', () => {
+// required array parameter
+const videos = [
+  {
+    node: {
+      name: 'video 1',
+      src: '#',
+      type: 'type',
+    },
+  },
+  {
+    node: {
+      name: 'video 1',
+      src: '#',
+      type: 'type',
+    },
+  },
+]
+
+describe('video', () => {
   it('renders correctly', () => {
-    const tree = renderer.create(<Video />).toJSON()
+    const tree = renderer.create(<video entries={videos} />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly', () => {
+    const tree = renderer.create(<video entries={videos} />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,32 +1,53 @@
-import React from 'react';
-import styles from './styles.module.styl';
-import video_ele_webm from './elephants_dream.webm';
-import video_ele_mp4 from './elephants_dream.mp4';
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './styles.module.styl'
 
-function Video() {
-    const videos = [
-        {
-            src: video_ele_webm,
-            type: "video/webm"
-        },
-        {
-            src: video_ele_mp4,
-            type: "video/mp4"
-        }
-    ]
+const videos = [
+  { name: 'video 1', src: '/videos/sample-video-1.webm', type: 'video/webm' },
+  { name: 'video 2', src: '/videos/sample-video-2.mp4', type: 'video/mp4' },
+]
 
+class Video extends React.Component {
+  constructor(props) {
+    super(props)
+    this.videoRefs = []
+  }
 
+  componentDidMount() {
+    this.videoRefs[0] && this.videoRefs[0].focus()
+  }
 
-
-    const displayVideos = videos.map((video, index) =>
-              <source src={video.src} type={video.type} />
-          )
-
-
+  render() {
     return (
-      <video className={styles.videoSection} playsInline autoPlay controls>
-        {displayVideos}
-      </video>
+      <section className={styles.videoSection}>
+        <div className={styles.videoContainer}>
+          {videos.map((video, index) => (
+            <video
+              className={styles.video}
+              key={index}
+              value={video.name}
+              ref={ref => (this.videoRefs[index] = ref)}
+              src={video.src}
+              type={video.type}
+              playsInline
+              controls
+              allowFullScreen
+              muted
+            />
+          ))}
+        </div>
+      </section>
     )
+  }
 }
+
+Video.propTypes = {
+  src: PropTypes.string,
+  playsInline: PropTypes.bool,
+  controls: PropTypes.bool,
+  allowFullScreen: PropTypes.bool,
+  muted: PropTypes.bool,
+  children: PropTypes.node,
+}
+
 export default Video
