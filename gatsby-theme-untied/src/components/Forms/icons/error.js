5325fc4d47d0a1298669f99b62e87880ac078ca9
Jan Winterfeld <jan@euphorika.hamburg>
2019-07-31 11:38:03 +0200

    Adjust form icons

--- gatsby-theme-untied/src/components/Forms/icons/error.js
+++ gatsby-theme-untied/src/components/Forms/icons/error.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const FormErrorIcon = () => {
+const FormErrorIcon = ({ width, height }) => {
   const svgString =
-    '<svg id="Ebene_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 133.5 135.4"><style>.st0{clip-path:url(#SVGID_2_)}.st1{fill:#e9426c}.st2{fill:#fff}</style><defs><path id="SVGID_1_" d="M34 35.8h65.4v63.8H34z"/></defs><clipPath id="SVGID_2_"><use xlink:href="#SVGID_1_" overflow="visible"/></clipPath><g class="st0"><path class="st1" d="M66.8 99.6c18.1 0 32.7-14.3 32.7-31.9S84.8 35.8 66.8 35.8 34 50.1 34 67.7s14.7 31.9 32.8 31.9"/></g><path class="st2" d="M81.7 54.6l-1.9-1.9-13 13.1-13.1-13.1-1.9 1.9 13 13.1-13 13.1 1.9 1.9 13.1-13.1 13 13.1 1.9-1.9-13-13.1z"/></svg>'
+    '<svg id="Ebene_1" data-name="Ebene 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.69 56.69"><defs><style>.cls-1{fill:#e9426c}.cls-2{fill:#fff}</style></defs><title>Gatsby-icons</title><path class="cls-1" d="M28.35 56.69A28.35 28.35 0 1 0 0 28.34a28.34 28.34 0 0 0 28.35 28.35"/><path class="cls-2" transform="rotate(-45 28.351 28.346)" d="M27.17 11.14h2.35v34.41h-2.35z"/><path class="cls-2" transform="rotate(-45 28.351 28.346)" d="M11.14 27.17h34.41v2.35H11.14z"/></svg>'
 
-  return <span dangerouslySetInnerHTML={{ __html: svgString }} />
+  return (
+    <span
+      style={{ display: 'inline-block', width: width, height: height }}
+      dangerouslySetInnerHTML={{ __html: svgString }}
+    />
+  )
+}
+
+FormErrorIcon.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+}
+
+FormErrorIcon.defaultProps = {
+  width: '100%',
+  height: 'auto',
 }
 
 export default FormErrorIcon
--- gatsby-theme-untied/src/components/Forms/icons/warning.js
+++ gatsby-theme-untied/src/components/Forms/icons/warning.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const FormWarningIcon = () => {
+const FormWarningIcon = ({ width, height }) => {
   const svgString =
-    '<svg id="Ebene_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 133.5 135.4"><style>.st0{fill:#e9426c}.st1{fill:#fff}</style><path class="st0" d="M66.8 99.6c18.1 0 32.7-14.3 32.7-31.9S84.8 35.8 66.8 35.8 34 50.1 34 67.7s14.7 31.9 32.8 31.9"/><path class="st1" d="M63.8 81.1c0-1.8.8-2.7 2.4-2.7.8 0 1.4.2 1.8.7s.6 1.1.6 2c0 .8-.2 1.5-.6 2-.4.5-1 .7-1.8.7-.7 0-1.3-.2-1.7-.6-.5-.4-.7-1.1-.7-2.1zm3.4-5.8h-2.1l-1-21h4.1l-1 21z"/></svg>'
+    '<svg id="Ebene_1" data-name="Ebene 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 56.69 56.69"><defs><style>.cls-1{fill:#f59d42}.cls-2{font-size:38.53px;fill:#fff;font-family:OpenSans-Regular,Open Sans}</style></defs><title>Gatsby-icons</title><path class="cls-1" d="M28.35 56.69A28.35 28.35 0 1 0 0 28.34a28.34 28.34 0 0 0 28.35 28.35"/><text class="cls-2" transform="translate(22.66 43.09)">!</text></svg>'
 
-  return <span dangerouslySetInnerHTML={{ __html: svgString }} />
+  return (
+    <span
+      style={{ display: 'inline-block', width: width, height: height }}
+      dangerouslySetInnerHTML={{ __html: svgString }}
+    />
+  )
+}
+
+FormWarningIcon.propTypes = {
+  width: PropTypes.string,
+  height: PropTypes.string,
+}
+
+FormWarningIcon.defaultProps = {
+  width: '100%',
+  height: 'auto',
 }
 
 export default FormWarningIcon
