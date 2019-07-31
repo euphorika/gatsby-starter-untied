727d09e56df38d2e14555a6316e1554a6f41eb8b
Jan Winterfeld <jan@euphorika.hamburg>
2019-07-31 11:44:50 +0200

    Create Icon test

---
+++ gatsby-theme-untied/src/components/Forms/__tests__/icon.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Icon from '../icon'
+
+describe('Icon', () => {
+  it('renders info correctly', () => {
+    const tree = renderer.create('<Icon type="info" />')
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders warn correctly', () => {
+    const tree = renderer.create('<Icon type="warn" />')
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders error correctly', () => {
+    const tree = renderer.create('<Icon type="error" />')
+    expect(tree).toMatchSnapshot()
+  })
+})
