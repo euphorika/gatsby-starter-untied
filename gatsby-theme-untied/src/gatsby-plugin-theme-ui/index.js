4c9b260a895a9f7a447364395f1303a06ac5a883
Jan Winterfeld <jan@euphorika.hamburg>
2019-08-06 10:25:27 +0200

    Shadow colors.js

--- gatsby-theme-untied/src/gatsby-plugin-theme-ui/index.js
+++ gatsby-theme-untied/src/gatsby-plugin-theme-ui/index.js
@@ -1,20 +1,9 @@
+import colors from './colors'
+
 export default {
   initialColorMode: 'light',
   useCustomProperties: true,
-  colors: {
-    text: '#3c3c3c',
-    background: '#f1f1f1',
-    primary: '#3c3c3c',
-    // secondary: '',
-    // accent: '',
-    // muted: '',
-    /*modes: {
-      dark: {
-        text: '#f1f1f1',
-        background: '#3c3c3c',
-      },
-    },*/
-  },
+  colors,
   fonts: {
     body: 'system-ui, sans-serif',
     heading: 'Georgia, serif',
---
+++ site/src/gatsby-theme-untied/gatsby-plugin-theme-ui/colors.js
@@ -0,0 +1,14 @@
+export default {
+  text: '#3c3c3c',
+  background: '#f1f1f1',
+  primary: '#3c3c3c',
+  // secondary: '',
+  // accent: '',
+  // muted: '',
+  modes: {
+    dark: {
+      text: '#f1f1f1',
+      background: '#3c3c3c',
+    },
+  },
+}
