50a9d2e7ce2b2ddf768cc536c0a0070a8daee1c1
Jan Winterfeld <jan@euphorika.hamburg>
2019-08-07 16:54:19 +0200

    get rid of hero section stylus module

--- gatsby-theme-untied/src/components/Hero/index.js
+++ gatsby-theme-untied/src/components/Hero/index.js
@@ -1,27 +1,80 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import PropTypes from 'prop-types'
 
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 
-import styles from './styles.module.styl'
-
 const Hero = ({ children, headline, button, imgFluid }) => (
-  <section className={styles.heroSection}>
-    <div className={styles.background}>
+  <section
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      position: 'relative',
+      width: '100vw',
+      height: '100vh',
+      color: 'secondary',
+      p: [3, 4],
+    }}
+  >
+    <div
+      sx={{
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+      }}
+    >
       <Img
         fluid={imgFluid}
         style={{ height: '100%' }}
         imgStyle={{ objectFit: 'cover' }}
       />
     </div>
-    <div className={styles.info}>
-      <h2>{headline}</h2>
-      <p>{children}</p>
+    <div
+      sx={{
+        position: 'relative',
+        maxWidth: '400px',
+        zIndex: '10',
+      }}
+    >
+      <h2
+        sx={{
+          fontSize: 2,
+          mb: 3,
+        }}
+      >
+        {headline}
+      </h2>
+      <p
+        sx={{
+          fontSize: 0,
+        }}
+      >
+        {children}
+      </p>
       <div>
-        <button className={styles.button}>
+        <button
+          sx={{
+            bg: 'primary',
+            width: '100%',
+            maxWidth: '300px',
+            border: 0,
+            display: 'inline-block',
+            borderRadius: 0,
+            fontSize: 0,
+            padding: 3,
+          }}
+        >
           <Link to={button.link}>
-            <span className={styles.text}>{button.text}</span>
+            <span
+              sx={{
+                color: 'secondary',
+              }}
+            >
+              {button.text}
+            </span>
           </Link>
         </button>
       </div>
