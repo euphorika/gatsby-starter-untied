c5149d474fcd641b314108b3d125ccf936672200
Hussein Bawab <hussein@markenbegeisterung.de>
2019-08-09 10:11:09 +0200

    get rid of CallToAction stylus module -> jsx

--- gatsby-theme-untied/src/components/CallToAction/index.js
+++ gatsby-theme-untied/src/components/CallToAction/index.js
@@ -1,13 +1,58 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
 import PropTypes from 'prop-types'
-import styles from './styles.module.styl'
 import { Link } from 'gatsby'
 
 const CallToAction = ({ button }) => (
-  <section className={styles.callToActionSection}>
-    <button className={styles.button}>
+  <section
+    sx={{
+      margin: 0,
+    }}
+  >
+    <button
+      sx={{
+        display: 'block',
+        borderRadius: 1,
+        bg: 'primary',
+        textAlign: 'center',
+        fontSize: 2,
+        fontWeight: '600',
+        padding: ['15px 100px', '20px 170px'],
+        marginTop: '20px',
+        marginBottom: '20px',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        '&:hover': {
+          opacity: '1',
+          right: '0',
+        },
+      }}
+    >
       <Link to={button.link}>
-        <span> {button.text} </span>
+        <span
+          sx={{
+            display: 'inline-block',
+            position: 'relative',
+            transition: '0.5s',
+            color: 'secondary',
+            '&:after': {
+              //content: '\00bb',
+              position: 'absolute',
+              top: '0',
+              right: '-20px',
+              transition: '0.5s',
+              opacity: '1',
+              right: '0',
+            },
+
+            '&:hover': {
+              paddingRight: '25px',
+            },
+          }}
+        >
+          {' '}
+          {button.text}{' '}
+        </span>
       </Link>
     </button>
   </section>
